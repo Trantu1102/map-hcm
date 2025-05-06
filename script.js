281b1e22a873462bb68408652ee7907409247d9d
Trantu1102 <163641356+Trantu1102@users.noreply.github.com>
2025-05-06 07:44:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -116,7 +116,6 @@ const showTooltip = (data, event) => {
         <div class="district-header">
             <h3>${data.name}</h3>
         </div>
-        <div class="district-list-scroll">
     `;
     data.districts.forEach(district => {
         tooltipContent += `
@@ -138,7 +137,6 @@ const showTooltip = (data, event) => {
             </div>
         `;
     });
-    tooltipContent += `</div>`;
     tooltip.innerHTML = tooltipContent;
     tooltip.classList.add('active'); // Luôn thêm class active cho cả desktop và mobile
     tooltip.style.display = 'block'; // Đảm bảo hiển thị
